7313f03364e7196547ba691027bd824beedc97f8
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-06 05:48:31 +0600

    decsion making

---
+++ decision_making.js
@@ -0,0 +1,17 @@
+var zahed={
+    good_side:"He love help people",
+    bad_side:"Too much emotional",
+    company:"Syscomatic Technologies",
+    role:"Chief Technology Officer"
+}
+
+if(zahed['good_side']=="He love help people"){
+    console.log("Yeah he is a good heart");
+}
+else if(zahed['good_side']=='hate'){
+    console.log("He is not a solid person!");
+}
+
+else{
+    console.log("He is a alien bhai")
+}
