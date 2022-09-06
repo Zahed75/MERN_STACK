53fbaf4834a2d7ac54091ce04e95d10219b1d424
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-07 05:51:14 +0600

    multidimensional array

---
+++ array/multidimensionalArray.js
@@ -0,0 +1,12 @@
+// akta array er moddhe onno onek gula array
+
+var bangladesh=["Dhaka","Shylet","Rangpur","India","Mumbai","Kolkata"]
+
+var india=["Mumbai","Delhi","Taminlnardu","Kashmir"]
+
+var china=["Uhan","Shanghai","Hongkong"]
+
+
+var asia=[bangladesh,india,china]
+
+console.log(asia[0][0]);
---
+++ array/simplearray.js
@@ -0,0 +1,9 @@
+var myarray=["Zahed","Lusaiba","Rakib","Tauhid","Shahed","Asif","Sabbir","Jisan"]
+
+var secarray=new Array("Zahed","Lusaiba","Rakib","Tauhid","Shahed","Asif","Sabbir","Jisan")
+
+console.log(myarray[1]);
+
+for(let item of myarray){
+    console.log(item);
+}
