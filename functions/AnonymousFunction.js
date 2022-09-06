217e4bb62c6ec14dabcb492ebcc760c39e06fb2d
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-07 05:34:37 +0600

    arrowfunction done

---
+++ functions/AnonymousFunction.js
@@ -0,0 +1,18 @@
+
+
+var x =function(x){
+    return x;
+}
+
+
+
+var x =function(x){
+    return "Hello we are anonymuous"
+}
+
+console.log(x());
+
+// Reassign
+
+
+
---
+++ functions/arrowfunction.js
@@ -0,0 +1,15 @@
+var myarrow=()=>{
+    console.log("My Arrow Function");
+}
+
+myarrow();
+
+// resparameter function with arrowfunctions
+
+
+var myarrowtwo=(...x)=>{
+    console.log(x);
+}
+
+
+myarrowtwo("Hello Syscomatic..Your CTO give you something...")
