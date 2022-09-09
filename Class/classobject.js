b0b40e798c4f70e4238eb7e6fb548ee6799dcd51
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-09 09:43:37 +0600

    data up

---
+++ Class/classobject.js
@@ -0,0 +1,42 @@
+// class er vitore kono value use korte chaile oi class er object make kore nite hobe
+
+class MyClass{
+
+MyFun(){
+    console.log("Im From Fun!")
+}
+
+Fun1(){
+    console.log("Im From Fun1")
+}
+
+Fun2(){
+    console.log("Im From Fun2")
+}
+
+}
+
+var obj=new MyClass;  //Creating a object on that class
+
+obj.Fun1(); //call the function
+
+
+// ===========================//
+
+
+class MyClass1{
+    Friend(name){
+        console.log(name);
+    }
+
+
+    university(name){
+        console.log(name);
+    }
+
+}
+
+
+var obj=new MyClass1;
+
+obj.Friend("Zahed Hasan")
---
+++ Class/constructor.js
@@ -0,0 +1,8 @@
+// constructor method alada kore call kore lage na just object make korle hocche
+class MyClass{
+    constructor(){
+        console.log("Hello Im From Constructor Class!");
+    }
+}
+
+new MyClass(); //object make
